7730a8b48a04221e8e416cfc086fa1c71d2c21ad
Kaktoose <72473270+Kaktoose@users.noreply.github.com>
2021-06-04 09:04:45 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function myFunction() {
+let first = prompt('what is your first number ');
+let second = prompt('what is your second number ');
+let answer = (first + second);
+console.log(answer);
+let give = prompt('the answer is ' + answer  + ' and is answer' );
+}
